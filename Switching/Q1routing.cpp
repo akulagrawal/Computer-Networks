a5c90fac9b05cda912fd2339af343ece87937596
Akul Agrawal <akulagrawal@rediffmail.com>
2020-02-21 19:39:25 +0530

    Add working code for problem 1

--- Switching/Q1routing.cpp
+++ Switching/Q1routing.cpp
@@ -86,20 +86,18 @@ int distance(topology top, bool flag){
     return 1;
 }
 
-vector<int> findRoute1(vector<vector<int> > dist, int i, int j, vector<vector<pii> > graph, map<pair<int, pii>, bool> &my){
+vector<int> findRoute1(vector<vector<int> > dist, int i, int j, vector<vector<int> > graph, bool flag, map<int, bool> &my){
     vector<int> v;
     int k=i;
-    v.pb(i);
     while(k!=j){
         int n=graph[k].size();
         for(int l=0;l<n;l++){
-            int x=graph[k][l].f;
-            if(dist[k][j]==dist[k][x]+dist[x][j]){
-                pair<int, pii> temp = mp(graph[k][l].s, mp(k,x));
-                my[temp] = 1;
-                temp.s = mp(x,k);
-                my[temp] = 1;
-                v.pb(x);
+            int x = topList[graph[k][l]].node1;
+            if(x == k)
+                x = topList[graph[k][l]].node2;
+            if(dist[k][j]==distance(topList[graph[k][l]], flag)+dist[x][j]){
+                my[graph[k][l]] = 1;
+                v.pb(graph[k][l]);
                 k=x;
                 break;
             }
@@ -119,13 +117,13 @@ void printDist(vector<vector<int> > dist){
 vector<vector<vector<vector<int> > > > findShortestPaths(bool flag){
 
     vector<vector<int> > dist(V, vector<int>(V, INF));
-    vector<vector<pii> > graph(V, vector<pii>());
+    vector<vector<int> > graph(V, vector<int>());
     vector<vector<vector<vector<int> > > > shortest(3, vector<vector<vector<int> > >(V, vector<vector<int> >(V, vector<int>())));
     for(int i=0;i<topList.size();i++){
         dist[topList[i].node1][topList[i].node2] = min(distance(topList[i], flag), dist[topList[i].node1][topList[i].node2]);
-        dist[topList[i].node2][topList[i].node1] = min(distance(topList[i], flag), dist[topList[i].node1][topList[i].node1]);
-        graph[topList[i].node1].pb(mp(topList[i].node2, distance(topList[i], flag)));
-        graph[topList[i].node2].pb(mp(topList[i].node1, distance(topList[i], flag)));
+        dist[topList[i].node2][topList[i].node1] = min(distance(topList[i], flag), dist[topList[i].node2][topList[i].node1]);
+        graph[topList[i].node1].pb(i);
+        graph[topList[i].node2].pb(i);
     }
 
     for(int i=0;i<V;i++)
@@ -146,32 +144,34 @@ vector<vector<vector<vector<int> > > > findShortestPaths(bool flag){
         for(int j=0;j<V;j++){
             if(i==j)
                 continue;
-            map<pair<int, pii>, bool> my;
-            shortest[0][i][j] = findRoute1(dist, i, j, graph, my);
+            map<int, bool> my;
+            shortest[0][i][j] = findRoute1(dist, i, j, graph, flag, my);
             int minm=INF;
             int v1=0, v2=0;
             bool isSecond=0;
+            int idx=0;
             for(int l=0;l<topList.size();l++){
-                pair<int, pii> temp1 = mp(distance(topList[l], flag), mp(topList[l].node1, topList[l].node2));
-                pair<int, pii> temp2 = mp(distance(topList[l], flag), mp(topList[l].node2, topList[l].node1));
-                if((!my[temp1])&&(!my[temp2])&&(topList[l].node1!=topList[l].node2)){
-                    if(minm > (temp1.f+dist[i][temp1.s.f]+dist[temp1.s.s][j])){
+                if((!my[l])&&(topList[l].node1!=topList[l].node2)){
+                    if(minm > (distance(topList[l], flag)+dist[i][topList[l].node1]+dist[topList[l].node2][j])){
                         isSecond = 1;
-                        minm = temp1.f+dist[i][temp1.s.f]+dist[temp1.s.s][j];
-                        v1 = temp1.s.f;
-                        v2 = temp1.s.s;
+                        minm = distance(topList[l], flag)+dist[i][topList[l].node1]+dist[topList[l].node2][j];
+                        v1 = topList[l].node1;
+                        v2 = topList[l].node2;
+                        idx = l;
                     }
-                    if(minm > (temp2.f+dist[i][temp2.s.f]+dist[temp2.s.s][j])){
+                    if(minm > (distance(topList[l], flag)+dist[i][topList[l].node2]+dist[topList[l].node1][j])){
                         isSecond = 1;
-                        minm = temp2.f+dist[i][temp2.s.f]+dist[temp2.s.s][j];
-                        v1 = temp2.s.f;
-                        v2 = temp2.s.s;
+                        minm = distance(topList[l], flag)+dist[i][topList[l].node2]+dist[topList[l].node1][j];
+                        v1 = topList[l].node2;
+                        v2 = topList[l].node1;
+                        idx = l;
                     }
                 }
             }
             if(isSecond){
-                vector<int> vec1 = findRoute1(dist, i, v1, graph, my);
-                vector<int> vec2 = findRoute1(dist, v2, j, graph, my);
+                vector<int> vec1 = findRoute1(dist, i, v1, graph, flag, my);
+                vector<int> vec2 = findRoute1(dist, v2, j, graph, flag, my);
+                vec1.pb(idx);
                 vec1.insert(vec1.end(), vec2.begin(), vec2.end());
                 shortest[1][i][j] = vec1;
             }
@@ -180,16 +180,17 @@ vector<vector<vector<vector<int> > > > findShortestPaths(bool flag){
     return shortest;
 }
 
-vector<int> findRoute2(vector<int> cost, int i, int j, vector<vector<int> > graph, vector<vector<int> > dist){
+vector<int> findRoute2(vector<int> cost, int i, int j, vector<vector<int> > graph, bool flag){
     vector<int> v;
     int k=j;
-    v.pb(j);
     while(k!=i){
         int n=graph[k].size();
         for(int l=0;l<n;l++){
-            int x=graph[k][l];
-            if(cost[k]==cost[x]+dist[x][k]){
-                v.pb(x);
+            int x = topList[graph[k][l]].node1;
+            if(x == k)
+                x = topList[graph[k][l]].node2;
+            if(cost[k]==cost[x]+distance(topList[graph[k][l]], flag)){
+                v.pb(graph[k][l]);
                 k=x;
                 break;
             }
@@ -202,35 +203,24 @@ vector<vector<vector<int> > > findLinkDisjointShortestPaths(bool flag, vector<ve
 
     vector<vector<vector<int> > > shortest(V, vector<vector<int> >(V, vector<int>()));
 
-    vector<vector<int> > oldDist(V, vector<int>(V, INF));
     vector<vector<int> > oldGraph(V, vector<int>());
     for(int i=0;i<topList.size();i++){
-        oldDist[topList[i].node1][topList[i].node2] = min(distance(topList[i], flag), oldDist[topList[i].node1][topList[i].node2]);
-        oldDist[topList[i].node2][topList[i].node1] = min(distance(topList[i], flag), oldDist[topList[i].node1][topList[i].node1]);
-        oldGraph[topList[i].node1].pb(topList[i].node2);
-        oldGraph[topList[i].node2].pb(topList[i].node1);
+        oldGraph[topList[i].node1].pb(i);
+        oldGraph[topList[i].node2].pb(i);
     }
 
-    for(int i=0;i<V;i++)
-        oldDist[i][i] = 0;
-
-    vector<vector<int> > dist(V, vector<int>(V, INF));
     vector<vector<int> > graph(V, vector<int>());
     for(int i=0;i<V;i++){
         for(int j=0;j<V;j++){
             if(!sPath[i][j].size())
                 continue;
             vector<int> cost(V, INF);
-            dist = oldDist;
             graph = oldGraph;
             vector<int> route = sPath[i][j];
-            dist[i][route[0]] = INF;
-            dist[route[0]][i] = INF;
-            for(int k=1;k<route.size();k++){
-                dist[route[k-1]][route[k]] = INF;
-                dist[route[k]][route[k-1]] = INF;
-                graph[route[k]].erase(remove(graph[route[k]].begin(), graph[route[k]].end(), route[k-1]), graph[route[k]].end());
-                graph[route[k-1]].erase(remove(graph[route[k-1]].begin(), graph[route[k-1]].end(), route[k]), graph[route[k-1]].end());
+            for(int k=0;k<route.size();k++){
+                topology temp = topList[route[k]];
+                graph[temp.node1].erase(remove(graph[temp.node1].begin(), graph[temp.node1].end(), route[k]), graph[temp.node1].end());
+                graph[temp.node2].erase(remove(graph[temp.node2].begin(), graph[temp.node2].end(), route[k]), graph[temp.node2].end());
             }
 
             priority_queue< pii, vector< pii >, greater< pii > > pq;
@@ -245,14 +235,17 @@ vector<vector<vector<int> > > findLinkDisjointShortestPaths(bool flag, vector<ve
                 if(cost[current] < w_cur) continue;
 
                 for(int j=0;j<graph[current].size();j++)
-                {   
-                    if(cost[graph[current][j]] > cost[current] + dist[current][graph[current][j]]){
-                        cost[graph[current][j]] = cost[current] + dist[current][graph[current][j]];
-                        pq.push(mp(cost[graph[current][j]], graph[current][j]));
+                {
+                    int x = topList[graph[current][j]].node1;
+                    if(current == x)
+                        x = topList[graph[current][j]].node2;
+                    if(cost[x] > cost[current] + distance(topList[graph[current][j]], flag)){
+                        cost[x] = cost[current] + distance(topList[graph[current][j]], flag);
+                        pq.push(mp(cost[x], x));
                     }
                 }
             }
-            shortest[j][i] = findRoute2(cost, i, j, graph, dist);
+            shortest[j][i] = findRoute2(cost, i, j, graph, flag);
         }
     }
 
@@ -263,16 +256,37 @@ void printPath(vector<vector<vector<vector<int> > > > shortest){
     for(int i=0;i<V;i++){
         for(int j=0;j<V;j++){
             cout<<"Path 1 from "<<i<<" to "<<j<<":\n";
-            for(int k=0;k<shortest[0][i][j].size();k++)
-                cout<<shortest[0][i][j][k]<<"->";
+            int l=i;
+            cout<<i<<"->";
+            for(int k=0;k<shortest[0][i][j].size();k++){
+                int x = topList[shortest[0][i][j][k]].node1;
+                if(x == l)
+                    x = topList[shortest[0][i][j][k]].node2;
+                l = x;
+                cout<<x<<"->";
+            }
             cout<<endl;
             cout<<"Path 2 from "<<i<<" to "<<j<<":\n";
-            for(int k=0;k<shortest[1][i][j].size();k++)
-                cout<<shortest[1][i][j][k]<<"->";
+            l=i;
+            cout<<i<<"->";
+            for(int k=0;k<shortest[1][i][j].size();k++){
+                int x = topList[shortest[1][i][j][k]].node1;
+                if(x == l)
+                    x = topList[shortest[1][i][j][k]].node2;
+                l = x;
+                cout<<x<<"->";
+            }
             cout<<endl;
             cout<<"Path 3 from "<<i<<" to "<<j<<":\n";
-            for(int k=0;k<shortest[2][i][j].size();k++)
-                cout<<shortest[2][i][j][k]<<"->";
+            l=i;
+            cout<<i<<"->";
+            for(int k=0;k<shortest[2][i][j].size();k++){
+                int x = topList[shortest[2][i][j][k]].node1;
+                if(x == l)
+                    x = topList[shortest[2][i][j][k]].node2;
+                l = x;
+                cout<<x<<"->";
+            }
             cout<<endl;
         }
     }
@@ -280,22 +294,20 @@ void printPath(vector<vector<vector<vector<int> > > > shortest){
 
 bool checkRoute(vector<int> route, connection con, bool p){
     if(p==0){
-        for(int i=1;i<route.size();i++){
-            topology temp = topList[edgeToTop[mp(route[i-1], route[i])]];
-            if(con.bequiv > temp.capacity - temp.bequivSum)
+        for(int i=0;i<route.size();i++){
+            if(con.bequiv > topList[route[i]].capacity - topList[route[i]].bequivSum)
                 return 0;
         }
-        for(int i=1;i<route.size();i++)
-            topList[edgeToTop[mp(route[i-1], route[i])]].bequivSum += con.bequiv;
+        for(int i=0;i<route.size();i++)
+            topList[route[i]].bequivSum += con.bequiv;
         return 1;
     }
-    for(int i=1;i<route.size();i++){
-        topology temp = topList[edgeToTop[mp(route[i-1], route[i])]];
-        if(con.bmax > temp.capacity - temp.bmaxSum)
+    for(int i=0;i<route.size();i++){
+        if(con.bmax > topList[route[i]].capacity - topList[route[i]].bmaxSum)
             return 0;
     }
-    for(int i=1;i<route.size();i++)
-        topList[edgeToTop[mp(route[i-1], route[i])]].bmaxSum += con.bmax;
+    for(int i=0;i<route.size();i++)
+        topList[route[i]].bmaxSum += con.bmax;
     return 1;
 }
 
@@ -337,41 +349,121 @@ vector<int> setupConnections(bool p, vector<vector<vector<vector<int> > > > shor
     return setupPessimistic(shortest);
 }
 
-vector<int> writeRoutingTable(vector<vector<vector<vector<int> > > > shortest){
+void writeRoutingTable(string filename, vector<vector<vector<vector<int> > > > shortest){
     fstream f;
-    f.open("routingtable.csv", ios::out);
     for(int i=0;i<V;i++){
-        f.open("routingtable_"+to_string(i)+".csv", ios::out);
+        string name = filename+"_"+to_string(i)+".csv";
+        f.open(name.c_str(), ios::out);
         f.close();
     }
+    map<pair<int,pair<pii,pii> >, bool> isEntry;
     for(int i=0;i<V;i++){
         for(int j=0;j<V;j++){
             vector<int> route = shortest[0][i][j];
+            int l=i;
             for(int k=0;k<route.size();k++){
-                f.open("routingtable_"+to_string(route[k])+".csv", ios::out, ios::app);
-                f<<route[k+1]<<","<<edgeToTop(mp(route[k], route[k+1]))<<","<<
+                int x = topList[route[k]].node1;
+                if(x == l)
+                    x = topList[route[k]].node2;
+                if(!isEntry[mp(l,mp(mp(x,route[k]),mp(topList[route[k]].propDelay,topList[route[k]].capacity)))]){
+                    string name = filename+"_"+to_string(l)+".csv";
+                    f.open(name.c_str(), ios::out | ios::app);
+                    f<<x<<","<<route[k]<<","<<topList[route[k]].propDelay<<","<<topList[route[k]].capacity<<"\n";
+                    f.close();
+                    isEntry[mp(l,mp(mp(x,route[k]),mp(topList[route[k]].propDelay,topList[route[k]].capacity)))] = 1;
+                }
+                l = x;
             }
         }
     }
 }
 
-int main()
+void writeForwardingTable(string filename, vector<int> v, vector<vector<vector<vector<int> > > > shortest){
+    fstream f;
+    for(int i=0;i<V;i++){
+        string name = filename+"_"+to_string(i)+".csv";
+        f.open(name.c_str(), ios::out);
+        f.close();
+    }
+    int port = 7000;
+    for(int i=0;i<v.size();i++){
+        if(v[i] == -1)
+            continue;
+        vector<int> route = shortest[v[i]][conList[i].source][conList[i].dest];
+        int k=conList[i].source;
+        for(int j=0;j<route.size();j++){
+            int x = topList[route[j]].node1;
+            if(x == k)
+                x = topList[route[j]].node2;
+            string name = filename+"_"+to_string(k)+".csv";
+            f.open(name.c_str(), ios::out | ios::app);
+                f<<k<<","<<port<<","<<i<<","<<port+1<<","<<i<<"\n";
+            f.close();
+            k = x;
+        }
+    }
+}
+
+void writePathsFile(string filename, vector<int> v, vector<vector<vector<vector<int> > > > shortest){
+    int total = v.size();
+    int admitted = 0;
+    fstream f;
+    f.open(filename+".txt", ios::out);
+    f.close();
+    f.open(filename+".csv", ios::out);
+    f.close();
+    f.open(filename+".csv", ios::out | ios::app);
+    for(int i=0;i<v.size();i++){
+        if(v[i] == -1)
+            continue;
+        admitted++;
+        f<<i<<","<<conList[i].source<<","<<conList[i].dest<<",";
+
+        vector<int> route = shortest[v[i]][conList[i].source][conList[i].dest];
+        int k=conList[i].source;    
+        for(int j=0;j<route.size();j++){
+            int x = topList[route[j]].node1;
+            if(x == k)
+                x = topList[route[j]].node2;
+            f<<k<<"-";
+            k = x;
+        }
+        f<<k<<","<<i<<"\n";
+    }
+    f.close();
+    f.open(filename+".txt", ios::out | ios::app);
+    f<<total<<","<<admitted<<"\n";
+    f.close();
+}
+
+int main(int argc, char** argv)
 {
     //freopen ("input.in","r",stdin);
-    freopen ("output.txt","w",stdout);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    //freopen ("output.txt","w",stdout);
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(NULL);
 
+    string outputDir = "output/";
     vector<vector<vector<vector<int> > > > shortest;
 
-    readConnectionData("ARPANET_100.doc");
-    readTopologyData("top24.doc");
-    shortest = findShortestPaths(1);
-    shortest[2] = findLinkDisjointShortestPaths(1, shortest[0]);
-    printPath(shortest);
-    vector<int> v = setupConnections(0, shortest);
-    for(int i=0;i<v.size();i++)
-        cout<<v[i]<<" ";
+    if(strcmp(argv[12], "hop") && strcmp(argv[12], "dist")){
+        cout<<"Incorrect flag value "<<argv[12]<<"\n";
+        exit(0);
+    }
+    if(strcmp(argv[14], "0") && strcmp(argv[14], "1")){
+        cout<<"Incorrect p value "<<argv[14]<<"\n";
+        exit(0);
+    }
+
+    readTopologyData(argv[2]);
+    readConnectionData(argv[4]);
+    shortest = findShortestPaths(argv[12] != "hop");
+    shortest[2] = findLinkDisjointShortestPaths(argv[12] != "hop", shortest[0]);
+    //printPath(shortest);
+    vector<int> v = setupConnections((bool)stoi(argv[14]), shortest);
+    writeRoutingTable(outputDir+argv[6], shortest);
+    writeForwardingTable(outputDir+argv[8], v, shortest);
+    writePathsFile(outputDir+argv[10], v, shortest);
 
     return 0;
 }
